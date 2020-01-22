ec669c8cc870dfec17c707f68ca81cd55ecf2a1f
Dernival Liandro <liandro.sys@gmail.com>
2020-01-21 21:31:54 -0300

    Solução #3 - Método bom para implementação de tratamento de exceções

---
+++ src/model/excepitions/DomainExcepition.java
@@ -0,0 +1,9 @@
+package model.excepitions;
+
+public class DomainExcepition extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DomainExcepition(String msg) {
+		super(msg);
+	}
+}
